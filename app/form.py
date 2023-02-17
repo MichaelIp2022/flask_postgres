86d8f6ee147e1f7d51d37bf7a4f07bf4a17953d6
MichaelIp2022 <114169457+MichaelIp2022@users.noreply.github.com>
2023-02-17 23:30:26 +0800

    Update form.py

--- app/form.py
+++ app/form.py
@@ -17,3 +17,8 @@ class RegistrationForm(FlaskForm):
         user = Registration.query.filter_by(email = email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address')
+    
+    def validate_username(self, username):
+	    user = Registration.query.filter_by(username = username.data).first()
+	    if user is not None:
+		    raise ValidationError('Please use a different email address')
